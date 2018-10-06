258bdd1d87ff69134355b5c61db983f36125a9e8
t-kusakabe <ttmac0704@gmail.com>
2018-10-06 17:52:09 +0900

    update indexjs

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,13 @@
 import React from 'react';
-export default () => <div>Hello world!</div>;
+import Head from 'next/head';
+import css from 'next/css';
+
+export default () => (
+  <div>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </head>
+
+    <h1>Hi. I am mobile-read!</h1>
+  </div>
+);
